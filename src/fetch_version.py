fc9cc0bf0048615434a748721dfdb381ecfba7a6
Abhishek Anand <76703822+abanand132@users.noreply.github.com>
2023-09-28 02:26:40 +0530

    Delete src/fetch_version.py

--- src/fetch_version.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def repo_version():
-    response = requests.get(url="https://raw.githubusercontent.com/abanand132/demo-repo/main/version.txt")
-    data = float(response.text)
-    return data
